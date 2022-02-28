1dae271c4c3c4d0ab2b73a2bef56066b29176326
Rafasana14 <rafasana9@gmail.com>
2022-02-28 19:20:12 +0100

    Task 018: Done.
    Cambiado example.com a example.net debido a fallo en la página.
    Eliminado última / en link de Iván.
    Dejado en comentario el test inicial.

--- src/test/java/acme/testing/FavouriteLinkTest.java
+++ src/test/java/acme/testing/FavouriteLinkTest.java
@@ -24,9 +24,45 @@ public class FavouriteLinkTest extends TestHarness {
 	@Test
 	@Order(10)
 	public void favouriteLink() {
+//		super.navigateHome();
+//		super.clickOnMenu("Anonymous", "Favourite link");		
+//		super.checkCurrentUrl("http://www.example.net");
+		this.favouriteLink("Favourite link");
+		this.favouriteLink("29533829N: Moreno Granado, Iván");
+		this.favouriteLink("80087089P: Sanabria Espárrago, Rafael");
+		this.favouriteLink("32914990N: Rivas Rosado, Miguel Ángel");
+		this.favouriteLink("49385635N: Martín Avecilla, Mariano");
+		this.favouriteLink("53586762M: Gómez Serrano, Dámaris");
+		this.favouriteLink("77860371N: Gómez Gómez, Miguel Ángel");
+	}
+	
+	private void favouriteLink(final String option) {
 		super.navigateHome();
-		super.clickOnMenu("Anonymous", "Favourite link");		
-		super.checkCurrentUrl("http://www.example.com");
+		super.clickOnMenu("Anonymous", option);
+		switch(option) {
+			case "Favourite link":
+				super.checkCurrentUrl("http://www.example.net");
+				break;
+			case "29533829N: Moreno Granado, Iván":
+				super.checkCurrentUrl("https://www.worldpadeltour.com");
+				break;
+			case "80087089P: Sanabria Espárrago, Rafael":
+				super.checkCurrentUrl("https://github.com/Rafasana14");
+				break;
+			case "32914990N: Rivas Rosado, Miguel Ángel":
+				super.checkCurrentUrl("https://github.com/migrivros");
+				break;
+			case "49385635N: Martín Avecilla, Mariano":
+				super.checkCurrentUrl("https://github.com/marmarave");
+				break;
+			case "53586762M: Gómez Serrano, Dámaris":
+				super.checkCurrentUrl("https://github.com/Damaris1008");
+				break;
+			case "77860371N: Gómez Gómez, Miguel Ángel":
+				super.checkCurrentUrl("https://lichess.org");
+				break;
+		}
+			
 	}
 	
 	// Ancillary methods ------------------------------------------------------ 
