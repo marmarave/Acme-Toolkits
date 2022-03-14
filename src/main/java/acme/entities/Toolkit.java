d4fa54521d690c6d6bca60695e71f5aeaa85a5bd
Rafasana14 <rafasana9@gmail.com>
2022-03-15 00:09:25 +0100

    Pre-release/0.3.0: Añadida extensión a AbstractEntity

--- src/main/java/acme/entities/Toolkit.java
+++ src/main/java/acme/entities/Toolkit.java
@@ -2,23 +2,27 @@ package acme.entities;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table
 @Getter
 @Setter
-public class Toolkit {
+public class Toolkit extends AbstractEntity{
 
 	// A toolkit may have several instances of the same component, but only one instance of a given tool.
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Column(unique = true)
 	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
 	private String code;
