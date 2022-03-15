10e2f8d1f92ffe56496004bac64f4175cb586a92
Ivan <ivan.moreno.granado@gmail.com>
2022-03-15 18:39:16 +0100

    Task 021: Corregido errores en anotaciones

--- src/main/java/acme/entities/Announcement.java
+++ src/main/java/acme/entities/Announcement.java
@@ -9,6 +9,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 
+import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
@@ -26,11 +27,11 @@ public class Announcement extends AbstractEntity{
 	protected Date creationMoment;
 	
 	@NotBlank
-	@Max(101)
+	@Length(min=1,max = 100)
 	protected String title;
 	
 	@NotBlank
-	@Max(256)
+	@Length(min=1,max=255)
 	protected String body;
 	
 	@Email
