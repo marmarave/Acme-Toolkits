ae11f06eff5c2a4b50b360414271b10636e883a0
[marmarave] <[marmarave@alum.us.es]>
2022-03-15 19:55:23 +0100

    Task022: Hecho

---
+++ src/main/java/acme/entities/ItemQuantity.java
@@ -0,0 +1,38 @@
+package acme.entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import acme.framework.entities.AbstractEntity;
+import acme.roles.Patron;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class ItemQuantity extends AbstractEntity{
+	
+	
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	@NotNull
+	@Min(1)
+	private Integer quantity;
+	
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Toolkit toolkit;
+	
+
+}
