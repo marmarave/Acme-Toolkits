c7e46d152a1e34b4bf61aed0404766602cbb7b49
[marmarave] <[marmarave@alum.us.es]>
2022-03-16 18:24:27 +0100

    Task028:Hecha

---
+++ src/main/java/acme/forms/AdministratorDashboard.java
@@ -0,0 +1,28 @@
+package acme.forms;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class AdministratorDashboard {
+	
+		// Serialisation identifier -----------------------------------------------
+
+		protected static final long	serialVersionUID	= 1L;
+
+		// Attributes -------------------------------------------------------------
+
+		Integer								totalNumComponents;
+		Map<String,List<Double>>			priceOfComponentsStats;
+		Map<String,List<Double>>			priceOfToolsStats;
+		Map<String,Integer>					numberOfPatronages;
+		Map<String,List<Double>>			patronagesStats;
+		
+
+
+}
