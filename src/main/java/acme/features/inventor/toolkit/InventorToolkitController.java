022d844225f4c069ccfed46732bf6d26bf98d0a2
Ivan <ivan.moreno.granado@gmail.com>
2022-04-19 12:28:26 +0200

    Task 043: Listar Toolkits por inventor HECHO

---
+++ src/main/java/acme/features/inventor/toolkit/InventorToolkitController.java
@@ -0,0 +1,33 @@
+package acme.features.inventor.toolkit;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+import acme.entities.Toolkit;
+import acme.framework.controllers.AbstractController;
+import acme.roles.Inventor;
+
+@Controller
+public class InventorToolkitController  extends AbstractController<Inventor, Toolkit>{
+	
+	// Internal state ---------------------------------------------------------
+	
+	@Autowired
+	protected InventorToolkitListMineService	listMineService;
+
+	@Autowired
+	protected InventorToolkitShowMineService	showMineService;
+	
+	// Constructors -----------------------------------------------------------
+
+
+		@PostConstruct
+		protected void initialise() {
+			super.addCommand("list-toolkits","list", this.listMineService);
+			super.addCommand("show-toolkits","show", this.showMineService);
+		}
+	
+}
