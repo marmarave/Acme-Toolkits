74f8b51d814b6fd50bb52dbf73f14709c2771a49
Rafasana14 <rafasana9@gmail.com>
2022-05-22 21:52:19 +0200

    Tarea de MoneyExchange: completada

--- src/main/java/acme/features/inventor/toolkit/InventorToolkitRepository.java
+++ src/main/java/acme/features/inventor/toolkit/InventorToolkitRepository.java
@@ -1,11 +1,13 @@
 package acme.features.inventor.toolkit;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.ItemQuantity;
+import acme.entities.MoneyExchangeCache;
 import acme.entities.Toolkit;
 import acme.framework.repositories.AbstractRepository;
 
@@ -20,4 +22,7 @@ public interface InventorToolkitRepository extends AbstractRepository {
 	
 	@Query("select iq from ItemQuantity iq where iq.toolkit.id = :masterId")
     Collection<ItemQuantity> findItemQuantitiesOfToolkit(int masterId);
+	
+	@Query("select c from MoneyExchangeCache c where c.source = :sourceCurrency and c.target=:targetCurrency")
+	Optional<MoneyExchangeCache> findCacheBySourceAndTarget(String sourceCurrency, String targetCurrency);
 }
--- src/main/webapp/WEB-INF/views/fragments/menu.jsp
+++ src/main/webapp/WEB-INF/views/fragments/menu.jsp
@@ -42,7 +42,9 @@
 			<acme:menu-suboption code="master.menu.authenticated.toolkit.list" action="/any/toolkit/list"/>
 			<acme:menu-separator/>
 			<acme:menu-suboption code="master.menu.authenticated.user-account.list" action="/any/user-account/list"/>
-			<acme:menu-suboption code="master.menu.authenticated.system-configuration.show" action="/authenticated/system-configuration/show"/>		
+			<acme:menu-suboption code="master.menu.authenticated.system-configuration.show" action="/authenticated/system-configuration/show"/>	
+			<acme:menu-separator/>
+			<acme:menu-suboption code="master.menu.authenticated.money-exchange" action="/authenticated/money-exchange/perform"/>	
 		</acme:menu-option>
 
 		<acme:menu-option code="master.menu.administrator" access="hasRole('Administrator')">
